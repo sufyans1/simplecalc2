26bf9fd615b7ad63372db913d3496ff13e20524e
muhammed sufyan <132143104+sufyans1@users.noreply.github.com>
2023-05-01 11:24:52 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -12,3 +12,11 @@ function btnResult(value)
 {
     screen.value=eval(screen.value);
 }
+function btnDelte()
+{
+    var strscreen=screen.value.toString();
+    const arrscreen=strscreen.split('');
+    arrscreen.pop();
+    strscreen=arrscreen.join("");
+    screen.value=strscreen;
+}
